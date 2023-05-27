3d1fc29c23fbce716c88eeb2da28c03cbe937460
JoreinhardS <jorensaragih@gmail.com>
2023-05-27 18:19:41 +1000

    Logo

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../../images/logo.png'
+import Logo from '../../images/Logo.png'
 import PrimaryButton from '../buttons/PrimaryButton'
 import Line from '../../images/line.svg'
 
